d733a5ae827bacde5c5971fada131e1f8912417e
Ezhilan M <mezhilan90@gmail.com>
2026-07-06 13:04:58 +0530

    Add files via upload

---
+++ day2/largestno.java
@@ -0,0 +1,18 @@
+package day2;
+import java.util.Scanner;
+public class largestno {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Number:");
+        int a = sc.nextInt();
+        int large = 0;
+        while (a != 0) {
+            int digit = a % 10;
+            if (digit > large) {
+                large = digit;
+            }
+            a = a / 10;
+        }
+        System.out.println(large);
+    }
+}
---
+++ day2/smalldigit.java
@@ -0,0 +1,20 @@
+package day2;
+import java.util.Scanner;
+public class smalldigit {
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a Number:");
+        int a=sc.nextInt();
+        int small=9;
+        while(a!=0){
+            int digit=a%10;
+            if(digit<small)
+            {
+                small=digit;
+            }
+            a=a/10;
+        }
+        System.out.println(small);
+    }
+}
